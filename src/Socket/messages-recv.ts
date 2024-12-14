e0d78f99c64b853292996604536f01bfa646c9c5
cifumo <mtiosaputra70@gmail.com>
2024-12-14 19:01:01 +0700

    update messages-recv.ts

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -112,6 +112,65 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		await sendNode(stanza)
 	}
 
+ const offerCall = async (toJid: string, isVideo: boolean = false) => {
+        const callId = (0, crypto_1.randomBytes)(16).toString('hex').toUpperCase().substring(0, 64);
+        const offerContent = [];
+        offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '16000' }, content: undefined });
+        offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '8000' }, content: undefined });
+        if (isVideo) {
+            offerContent.push({
+                tag: 'video',
+                attrs: {
+                    orientation: '0',
+                    'screen_width': '1920',
+                    'screen_height': '1080',
+                    'device_orientation': '0',
+                    enc: 'vp8',
+                    dec: 'vp8',
+                }
+            });
+        }
+        offerContent.push({ tag: 'net', attrs: { medium: '3' }, content: undefined });
+        offerContent.push({ tag: 'capability', attrs: { ver: '1' }, content: new Uint8Array([1, 4, 255, 131, 207, 4]) });
+        offerContent.push({ tag: 'encopt', attrs: { keygen: '2' }, content: undefined });
+        const encKey = (0, crypto_1.randomBytes)(32);
+        const devices = (await getUSyncDevices([toJid], true, false)).map(({ user, device }) => (0, WABinary_1.jidEncode)(user, 's.whatsapp.net', device));
+        await assertSessions(devices, true);
+        const { nodes: destinations, shouldIncludeDeviceIdentity } = await createParticipantNodes(devices, {
+            call: {
+                callKey: encKey
+            }
+        });
+        offerContent.push({ tag: 'destination', attrs: {}, content: destinations });
+        if (shouldIncludeDeviceIdentity) {
+            offerContent.push({
+                tag: 'device-identity',
+                attrs: {},
+                content: (0, Utils_1.encodeSignedDeviceIdentity)(authState.creds.account, true)
+            });
+        }
+        const stanza = ({
+            tag: 'call',
+            attrs: {
+                to: toJid,
+            },
+            content: [{
+                    tag: 'offer',
+                    attrs: {
+                        'call-id': callId,
+                        'call-creator': authState.creds.me.id,
+                    },
+                    content: offerContent,
+                }],
+        });
+        await query(stanza);
+        return {
+            callId,
+            toJid,
+            isVideo,
+        };
+    };
+    
 	const rejectCall = async(callId: string, callFrom: string) => {
 		const stanza: BinaryNode = ({
 			tag: 'call',
@@ -912,6 +971,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		...sock,
 		sendMessageAck,
 		sendRetryRequest,
+		offerCall,
 		rejectCall
 	}
 }
