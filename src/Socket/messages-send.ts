122c643cf35b2ae9a25a069cf71903d4b2264e9a
kangfarrel <163698767+kangfarrel@users.noreply.github.com>
2024-12-23 10:06:08 +0700

    Add files via upload

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -308,7 +308,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 	const relayMessage = async(
 		jid: string,
 		message: proto.IMessage,
-		{ messageId: msgId, participant, additionalAttributes, additionalNodes, useUserDevicesCache, cachedGroupMetadata, statusJidList }: MessageRelayOptions
+		{ messageId: msgId, participant, additionalAttributes, useUserDevicesCache, cachedGroupMetadata, statusJidList, additionalNodes }: MessageRelayOptions
 	) => {
 		const meId = authState.creds.me!.id
 
