5131782947b0877e2813337f9a8691fa4be43df1
kangfarrel <163698767+kangfarrel@users.noreply.github.com>
2024-12-23 10:37:19 +0700

    Add files via upload

--- src/Types/Message.ts
+++ src/Types/Message.ts
@@ -206,10 +206,6 @@ export type MessageRelayOptions = MinimalRelayOptions & {
     participant?: { jid: string, count: number }
     /** additional attributes to add to the WA binary node */
     additionalAttributes?: { [_: string]: string }    
-    /** additional attributes to add to the WA binary node */
-    additionalAttributes?: {
-        [_: string]: string;
-    };
     additionalNodes?: BinaryNode[];
     /** should we use the devices cache, or fetch afresh from the server; default assumed to be "true" */
     useUserDevicesCache?: boolean
