9aa8610e766e4ebd4561ecfd5bd45ad1a8906b76
kangfarrel <163698767+kangfarrel@users.noreply.github.com>
2024-12-24 17:46:56 +0700

    Add files via upload

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -547,6 +547,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 				if(additionalNodes && additionalNodes.length > 0) {
                       (stanza.content as BinaryNode[]).push(...additionalNodes);
                 } else {
+                   if(!isJidNewsLetter(jid)) {
                       (stanza.content as BinaryNode[]).push({
 						tag: 'biz',
 						attrs: {},
@@ -562,7 +563,8 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 							}]
     					}]
 				    });
-                }
+				  }
+               }
 
 				const buttonType = getButtonType(message)
 				if(buttonType) {
